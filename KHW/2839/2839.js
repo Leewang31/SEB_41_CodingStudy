30f1858fb885c8b9914a3e6b54c526cfc4566ff6
kimhw7 <khw786678@gmail.com>
2022-11-02 20:40:33 +0900

    2839 문제풀이

--- KHW/2839/2839.js
+++ KHW/2839/2839.js
@@ -5,7 +5,7 @@ let input = fs.readFileSync(filePath).toString().split(' ');
 input = Number(input);
 
 // input이 0이 될때까지 빼기
-// 5로 먼저 빼다가 나머지가 10보다 작고 9나 6이나 3이면 3으로 빼주기
+// 5로 먼저 빼다가 input이 15보다 작고 9나 6이나 3이면 3으로 빼주기
 let count = 0;
 while(input > 0) {
   if(input < 15 && input % 3 === 0) {
