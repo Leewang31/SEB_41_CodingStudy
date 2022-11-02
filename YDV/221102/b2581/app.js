0ec4d79e534b1f5839df910ae0c57abf2580d68e
niv_ad <didekqls93@gmail.com>
2022-11-02 19:27:14 +0900

    제출 ㅠㅠ

--- YDV/221102/b2581/app.js
+++ YDV/221102/b2581/app.js
@@ -65,37 +65,57 @@ input = input.map((el)=> +el)
 
 
 //const input = ['64', '65'];
-const primeNumArr = [];
-let primeNumSum = 0;
+// const primeNumArr = [];
+// let primeNumSum = 0;
  
-function primeNumber(n) {
-if (n < 2) {
-return;
-}
+// function primeNumber(n) {
+//     if (n < 2) {
+//         return;
+//     }
  
-for (let i = 2; i < n; i++) {
-if (n % i === 0) {
-return;
-}
-}
-primeNumArr.push(n);
-primeNumSum += n;
-}
+//     for (let i = 2; i < n; i++) {
+//         if (n % i === 0) {
+//         return;
+//             }
+//     }
+// primeNumArr.push(n);
+// primeNumSum += n;
+// }
  
-const begin = parseInt(input.shift());
-const end = parseInt(input.shift());
+// const begin = parseInt(input.shift());
+// const end = parseInt(input.shift());
  
-for (let i = begin; i <= end; i++) {
-primeNumber(i);
-}
+//  for (let i = begin; i <= end; i++) {
+//         primeNumber(i);
+//     }
  
-if (!primeNumArr.length) {
-console.log(-1);
-} else {
-console.log(primeNumSum);
-console.log(Math.min.apply(null, primeNumArr));
-}
+//     if (!primeNumArr.length) {
+// console.log(-1);
+// } else {
+// console.log(primeNumSum);
+// console.log(Math.min.apply(null, primeNumArr));
+// }
 // //풀리는데 안 넘어감 
 
+const [M, N] = input.map(item => Number(item));
+let primeNumbers = [];
+let sum = 0;
 
-Object.assign({}, state, {cartItems: [...state.cartItems, action.payload]})
+for (let check = M; check <= N; check++) {
+  for (let i = 2; i <= check; i++) {
+    if (i * i > check) {
+      primeNumbers.push(check);
+      sum += check;
+      break;
+    }
+    if (check % i === 0) {
+      break;
+    }
+  }
+}
+if (primeNumbers.length === 0) {
+  console.log(-1);
+} else {
+  console.log(sum);
+  console.log(primeNumbers[0]);
+}
