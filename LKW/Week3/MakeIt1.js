b837350059a399e7c7e72e44fb5b98037b0e1fc9
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-11-14 20:01:45 +0900

    [BOJ]1463/success

--- LKW/Week3/MakeIt1.js
+++ LKW/Week3/MakeIt1.js
@@ -5,26 +5,15 @@
 let fs = require('fs');
 let input = Number(fs.readFileSync('./dev/1463').toString());   //2
 
-let result = input
-let count = 0;
-const func = (n,count) => {
-    if(n===1){
-        console.log('final',count)
-        result = Math.min(count,result)
+const arr = new Array(input + 1).fill(0)
+for (let i = 2; i <= input; i++) {
+    // console.log(i)
+    arr[i] = arr[i - 1] + 1
+    if (i % 2 === 0) {
+        arr[i] = Math.min(arr[i], arr[i / 2] + 1)
     }
-    else{
-        if(n%3===0){
-            console.log('n%3===0: ',n)
-            func(n/3,count+1)
-        }
-        if(n%2===0){
-            console.log('n%2===0: ',n)
-            func(n/2,count+1)
-        }
-        console.log('n-1: ',n)
-        func(n-1,count+1)
+    if (i % 3 === 0) {
+        arr[i] = Math.min(arr[i], arr[i / 3] + 1)
     }
 }
-
-func(input,count)
-console.log(result)
+console.log(arr[input])
