2b815e5b976789925b384596be8819405e5ace52
djwjfk <grw07236@naver.com>
2022-12-14 17:06:50 +0900

    7주차 문제풀이 제출

--- CYJ/week_6/10815.js
+++ CYJ/week_6/10815.js
@@ -3,12 +3,13 @@ let input = require("fs").readFileSync("week_6/10815.txt").toString().trim().spl
 const cardList = input[1].split(" ").map((v) => Number(v));
 const haveCard = input[3].split(" ").map((v) => Number(v));
 
-let result = [];
-cardList.sort((a, b) => a - b); //오름차순 정렬
+
+let result = []; //결과값이 들어갈 빈 배열
+cardList.sort((a, b) => a - b); //오름차순 정렬 [-10,2,3,6,10]
 haveCard.forEach((card) => {
   let min = 0;
   let max = cardList.length - 1;
-  let res = false;
+  let res = false; //숫자카드를 가지고있는지를 확인=> false라면 0, true면 1
   while (min <= max) {
     let mid = parseInt((min + max) / 2);
     if (card < cardList[mid]) {
